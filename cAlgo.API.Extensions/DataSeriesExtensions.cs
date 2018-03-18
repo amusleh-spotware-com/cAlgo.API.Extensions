a833f054a0844df5b561d5ffaee778abde01fa39
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2018-03-18 15:49:49 +0430

    Maximum and Minimum methods added to DataSeriesExtensions class.

--- cAlgo.API.Extensions/DataSeriesExtensions.cs
+++ cAlgo.API.Extensions/DataSeriesExtensions.cs
@@ -11,6 +11,11 @@ namespace cAlgo.API.Extensions
 {
     public static class DataSeriesExtensions
     {
+        /// <summary>
+        /// Creates a List<double> from a DataSeries
+        /// </summary>
+        /// <param name="dataSeries"></param>
+        /// <returns>List<double></returns>
         public static List<double> ToList(this DataSeries dataSeries)
         {
             List<double> data = new List<double>();
@@ -21,6 +26,44 @@ namespace cAlgo.API.Extensions
             }
 
             return data;
+        }
+
+        /// <summary>
+        /// Returns the maximum value between start and end index in a dataseries
+        /// </summary>
+        /// <param name="dataSeries"></param>
+        /// <param name="startIndex">Start index (Ex: 1)</param>
+        /// <param name="endIndex">End index (Ex: 10)</param>
+        /// <returns>double</returns>
+        public static double Maximum(this DataSeries dataSeries, int startIndex, int endIndex)
+        {
+            double max = double.NegativeInfinity;
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                max = Math.Max(dataSeries[i], max);
+            }
+
+            return max;
+        }
+
+        /// <summary>
+        /// Returns the minimum value between start and end index in a dataseries
+        /// </summary>
+        /// <param name="dataSeries"></param>
+        /// <param name="startIndex">Start index (Ex: 1)</param>
+        /// <param name="endIndex">End index (Ex: 10)</param>
+        /// <returns>double</returns>
+        public static double Minimum(this DataSeries dataSeries, int startIndex, int endIndex)
+        {
+            double min = double.PositiveInfinity;
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                min = Math.Min(dataSeries[i], min);
+            }
+
+            return min;
         }
     }
 }
