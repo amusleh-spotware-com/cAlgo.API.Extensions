e615cd4b57e434e4f95661ce0aa4d98620699763
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2018-03-18 15:32:39 +0430

    ToList method added to DataSeriesExtensions class.

---
+++ cAlgo.API.Extensions/DataSeriesExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cAlgo.API;
+using cAlgo.API.Internals;
+using cAlgo.API.Indicators;
+
+namespace cAlgo.API.Extensions
+{
+    public static class DataSeriesExtensions
+    {
+        public static List<double> ToList(this DataSeries dataSeries)
+        {
+            List<double> data = new List<double>();
+
+            for (int i = 0; i < dataSeries.Count; i++)
+            {
+                data.Add(i);
+            }
+
+            return data;
+        }
+    }
+}
