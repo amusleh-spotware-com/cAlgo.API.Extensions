c84d759b7b07f6cf98230fb5b478614abc26fa7a
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2018-03-18 17:24:37 +0430

    New methods added to DataSeriesExtensions class.

--- cAlgo.API.Extensions/DataSeriesExtensions.cs
+++ cAlgo.API.Extensions/DataSeriesExtensions.cs
@@ -29,7 +29,7 @@ namespace cAlgo.API.Extensions
         }
 
         /// <summary>
-        /// Returns the maximum value between start and end index in a dataseries
+        /// Returns the maximum value between start and end (inclusive) index in a dataseries
         /// </summary>
         /// <param name="dataSeries"></param>
         /// <param name="startIndex">Start index (Ex: 1)</param>
@@ -48,7 +48,7 @@ namespace cAlgo.API.Extensions
         }
 
         /// <summary>
-        /// Returns the minimum value between start and end index in a dataseries
+        /// Returns the minimum value between start and end (inclusive) index in a dataseries
         /// </summary>
         /// <param name="dataSeries"></param>
         /// <param name="startIndex">Start index (Ex: 1)</param>
@@ -64,6 +64,80 @@ namespace cAlgo.API.Extensions
             }
 
             return min;
+        }
+
+        /// <summary>
+        /// Checks if the index value is higher than x (periods) previous values in a dataseries
+        /// </summary>
+        /// <param name="dataSeries"></param>
+        /// <param name="index">Dataseries value index</param>
+        /// <param name="periods">The number of index previous values to check</param>
+        /// <param name="bothSides">Check both previous and next values (by default it checks only previous values)</param>
+        /// <returns>bool</returns>
+        public static bool IsHigherHigh(this DataSeries dataSeries, int index, int periods, bool bothSides = false)
+        {
+            if (!bothSides)
+            {
+                return dataSeries[index] > dataSeries.Maximum(index - periods, index - 1);
+            }
+            else
+            {
+                bool leftSideResult = dataSeries[index] > dataSeries.Maximum(index - periods, index - 1);
+                bool rightSideResult = dataSeries[index] > dataSeries.Maximum(index + 1, index + periods);
+
+                return leftSideResult && rightSideResult;
+            }
+        }
+
+        /// <summary>
+        /// Checks if the index value is lower than x (periods) previous values in a dataseries
+        /// </summary>
+        /// <param name="dataSeries"></param>
+        /// <param name="index">Dataseries value index</param>
+        /// <param name="periods">The number of index previous values to check</param>
+        /// <param name="bothSides">Check both previous and next values (by default it checks only previous values)</param>
+        /// <returns>bool</returns>
+        public static bool IsLowerLow(this DataSeries dataSeries, int index, int periods, bool bothSides = false)
+        {
+            if (!bothSides)
+            {
+                return dataSeries[index] < dataSeries.Minimum(index - periods, index - 1);
+            }
+            else
+            {
+                bool leftSideResult = dataSeries[index] < dataSeries.Minimum(index - periods, index - 1);
+                bool rightSideResult = dataSeries[index] < dataSeries.Minimum(index + 1, index + periods);
+
+                return leftSideResult && rightSideResult;
+            }
+        }
+
+        /// <summary>
+        /// Returns the distance of index (parameter) value with highest value in it's x (periods) previous values in a dataseries
+        /// </summary>
+        /// <param name="dataSeries"></param>
+        /// <param name="index">Dataseries value index</param>
+        /// <param name="periods">The number of index previous values to check</param>
+        /// <returns>double</returns>
+        public static double GetDistanceWithHigherHigh(this DataSeries dataSeries, int index, int periods)
+        {
+            double highValue = dataSeries.Maximum(index - periods, index - 1);
+
+            return dataSeries[index] - highValue;
+        }
+
+        /// <summary>
+        /// Returns the distance of index (parameter) value with lowest value in it's x (periods) previous values
+        /// </summary>
+        /// <param name="dataSeries"></param>
+        /// <param name="index">Dataseries value index</param>
+        /// <param name="periods">The number of index previous values to check</param>
+        /// <returns>double</returns>
+        public static double GetDistanceWithLowerLow(this DataSeries dataSeries, int index, int periods)
+        {
+            double lowValue = dataSeries.Minimum(index - periods, index - 1);
+
+            return dataSeries[index] - lowValue;
         }
     }
 }
