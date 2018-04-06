56df869be0fcf657c1f085ab541709fedf5c7040
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2018-04-06 19:05:21 +0430

    GetPercentageChange method added to MarketSeriesExtensions class.

--- cAlgo.API.Extensions/MarketSeriesExtensions.cs
+++ cAlgo.API.Extensions/MarketSeriesExtensions.cs
@@ -17,6 +17,14 @@ namespace cAlgo.API.Extensions
             return marketSeries.Close.Count - 1;
         }
 
+        /// <summary>
+        /// Returns the volume profile of x latest values in a market series
+        /// </summary>
+        /// <param name="marketSeries"></param>
+        /// <param name="periods">The number of latest values</param>
+        /// <param name="priceStep">The step (Pips) that is used for increament of price</param>
+        /// <param name="symbol">The market series symbol</param>
+        /// <returns>List<PriceVolume></returns>
         public static List<PriceVolume> GetPriceVolume(this MarketSeries marketSeries, int periods, double priceStep, Symbol symbol)
         {
             List<PriceVolume> result = new List<PriceVolume>();
@@ -60,6 +68,17 @@ namespace cAlgo.API.Extensions
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// Returns the amount of percentage change of a value in comparison with it's previous value in a market series
+        /// </summary>
+        /// <param name="marketSeries"></param>
+        /// <param name="index">The value index</param>
+        /// <returns>double</returns>
+        public static double GetPercentageChange(this MarketSeries marketSeries, int index)
+        {
+            return -((marketSeries.Open[index] - marketSeries.Close[index]) / marketSeries.Open[index]) * 100;
         }
     }
 }
