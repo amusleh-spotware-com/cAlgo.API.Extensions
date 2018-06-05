ac0d2a3b4fb874d707f5e5f97ebf422f54cd0855
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2018-06-05 11:33:58 +0430

    PositionExtensions added.

---
+++ cAlgo.API.Extensions/PositionExtensions.cs
@@ -0,0 +1,41 @@
+﻿using cAlgo.API.Internals;
+
+namespace cAlgo.API.Extensions
+{
+    public static class PositionExtensions
+    {
+        /// <summary>
+        /// Returns a position stop loss in pips
+        /// </summary>
+        /// <param name="position">Position</param>
+        /// <param name="positionSymbol">Position symbol</param>
+        /// <returns>double</returns>
+        public static double? GetStopLossInPips(this Position position, Symbol positionSymbol)
+        {
+            double entryPrice = position.EntryPrice;
+
+            double? stopLossPrice = position.StopLoss;
+
+            double? stopLossPips = position.TradeType == TradeType.Buy ? entryPrice - stopLossPrice : stopLossPrice - entryPrice;
+
+            return stopLossPips.HasValue ? (double?)positionSymbol.ToPips(stopLossPips.Value) : null;
+        }
+
+        /// <summary>
+        /// Returns a position take profit in pips
+        /// </summary>
+        /// <param name="position">Position</param>
+        /// <param name="positionSymbol">Position symbol</param>
+        /// <returns>double</returns>
+        public static double? GetTakeProfitInPips(this Position position, Symbol positionSymbol)
+        {
+            double entryPrice = position.EntryPrice;
+
+            double? takeProfitPrice = position.TakeProfit;
+
+            double? takeProfitPips = position.TradeType == TradeType.Buy ? takeProfitPrice - entryPrice : entryPrice - takeProfitPrice;
+
+            return takeProfitPips.HasValue ? (double?)positionSymbol.ToPips(takeProfitPips.Value) : null;
+        }
+    }
+}
