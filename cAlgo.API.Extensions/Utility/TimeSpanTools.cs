e53582492aad491d28edcb3a523ed99cde923774
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2019-03-07 21:23:11 +0300

    TimeSpanTools TryParse method arguments position changed

--- cAlgo.API.Extensions/Utility/TimeSpanTools.cs
+++ cAlgo.API.Extensions/Utility/TimeSpanTools.cs
@@ -8,7 +8,7 @@ namespace cAlgo.API.Extensions.Utility
 {
     public static class TimeSpanTools
     {
-        public static void TryParse(string value, Chart chart, string parameterName, string chartObjectsSuffix, out TimeSpan result)
+        public static void TryParse(string value, string parameterName, Chart chart, string chartObjectsSuffix, out TimeSpan result)
         {
             bool parseResult = TimeSpan.TryParse(value, CultureInfo.InvariantCulture, out result);
 
