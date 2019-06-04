8fdd78bc59d1b562ac4215461bed0c0544d752e1
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2019-06-04 15:42:36 +0300

    GetVolume added

--- cAlgo.API.Extensions/SymbolExtensions.cs
+++ cAlgo.API.Extensions/SymbolExtensions.cs
@@ -86,5 +86,18 @@ namespace cAlgo.API.Extensions
         {
             return riskPercentage / (((symbol.PipValue / accountBalance) * 100.0) * volume);
         }
+
+        /// <summary>
+        /// Returns the amount of volume based on your provided risk percentage and stop loss
+        /// </summary>
+        /// <param name="symbol">The symbol</param>
+        /// <param name="riskPercentage">Risk percentage amount</param>
+        /// <param name="accountBalance">The account balance</param>
+        /// <param name="stopLossInPips">Stop loss amount in Pips</param>
+        /// <returns>double</returns>
+        public static double GetVolume(this Symbol symbol, double riskPercentage, double accountBalance, double stopLossInPips)
+        {
+            return riskPercentage / (((Math.Abs(stopLossInPips) * symbol.PipValue) / accountBalance) * 100);
+        }
     }
 }
