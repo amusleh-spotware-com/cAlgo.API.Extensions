387a7f4eafc09ae5cb53ca3b1d22fc2ec84d0821
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2020-12-03 16:38:38 +0300

    Added ChartExtensions

---
+++ cAlgo.API.Extensions/ChartExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace cAlgo.API.Extensions
+{
+    public static class ChartExtensions
+    {
+        public static void ScrollX(this Chart chart, TimeSpan interval)
+        {
+            var visibleBarTime = chart.Bars.GetOpenTime(interval > TimeSpan.Zero 
+                ? chart.LastVisibleBarIndex
+                : chart.FirstVisibleBarIndex);
+
+            var scrollBarTime = visibleBarTime.Add(interval);
+
+            chart.ScrollXTo(scrollBarTime);
+        }
+    }
+}
